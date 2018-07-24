377bd66cec0ea9e96ad209503de2f37a33be87f9
jmaloney <jnmaloney@gmail.com>
2018-07-24 10:33:20 +1000

    WebCore Update

--- main.cpp
+++ main.cpp
@@ -9,41 +9,72 @@
 #include "InputSystem.h"
 #include "RenderSystem.h"
 #include "CityMap.h"
+#include "RenderQueue.h"
+#include "WindowManager.h"
+#include "MenuManager.h"
+#include "MenuBuild.h"
+#include "CityMapRenderer.h"
+#include "imgui.h"
 
 
-GLFWwindow* g_window;
 RenderSystem* g_rs = 0;
 InputSystem* g_is = 0;
 CityMap g_map;
 
+WindowManager g_windowManager;
+MenuManager g_menuManager;
+MenuBuild g_menuBuild;
+CityMapRenderer g_mapRenderer;
 
+
+//
+// Drawing
+//
 void draw()
 {
-  //
-  // Drawing
-  //
-  g_rs->draw();
+  g_rs->start();
+  //g_renderQueue.draw(g_rs->uniformML, g_rs->uniformDiffuse);
+  g_mapRenderer.tick();
+  g_mapRenderer.m_renderQueue.draw(g_rs->uniformML, g_rs->uniformDiffuse);
+  g_mapRenderer.m_renderQueueAdd.draw(g_rs->uniformML, g_rs->uniformDiffuse);
+  g_rs->end();
+
+  g_menuManager.predraw();
+  g_menuBuild.drawHeader();
+  g_menuBuild.draw();
+  g_menuManager.postdraw(g_windowManager);
 }
 
 
+//
+// Input
+//
 void input()
 {
-  //
-  // Input
-  //
-  g_is->frame();
+}
+
+
+void window_size_callback(GLFWwindow* window, int width, int height)
+{
+  g_windowManager.width = width;
+  g_windowManager.height = height;
+
+  //g_rs->setWindow(width, height);
 }
 
 
 void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos)
 {
+  if (ImGui::GetIO().WantCaptureMouse) return;
+
   g_is->cursor_pos_callback(xpos, ypos);
+  g_rs->setCursor(xpos, ypos);
 }
 
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
-    g_is->mouse_button_callback(button, action, mods);
+  g_is->mouse_button_callback(button, action, mods);
 }
 
 
@@ -56,6 +87,25 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 
 void loop()
 {
+  // Check for a resize..
+  // int h = 0;
+  // int w = 0;
+  // int f = 0;
+  // emscripten_get_canvas_size(&w, &h, &f);
+  // if (
+  //   w != g_windowManager.width ||
+  //   h != g_windowManager.height)
+  //   {
+  //     glfwSetWindowSize(g_windowManager.g_window, w, h);
+  //     return;
+  //   }
+
+    //emscripten_set_canvas_size(200, 300);
+
+  //
+  // Normal operation:
+  //
+  //You can read the 'io.WantCaptureMouse', 'io.WantCaptureKeyboard' and 'io.WantTextInput' flags from the ImGuiIO structure (e.g. if (ImGui::GetIO().WantCaptureMouse) { ... } )
   input();
   draw();
 }
@@ -63,39 +113,70 @@ void loop()
 
 int init()
 {
-  if( !glfwInit() )
-  {
-      fprintf( stderr, "Failed to initialize GLFW\n" );
-      return 1;
-  }
+  g_windowManager.init("Civille");
+  g_menuManager.init(g_windowManager);
 
-  glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
-
-  // Open a window and create its OpenGL context
-  g_window = glfwCreateWindow( 1280, 720, "The Mayor", NULL, NULL);
-  if( g_window == NULL )
-  {
-      fprintf( stderr, "Failed to open GLFW window.\n" );
-      glfwTerminate();
-      return -1;
-  }
-  glfwMakeContextCurrent(g_window); // Initialize GLEW
-
-  // Create game objects
-  RenderSystem* rs = new RenderSystem();
-  g_rs = rs;
-  g_rs->setCityMap(&g_map);
+  g_rs = new RenderSystem();
+  g_rs->init(g_windowManager);
+  //g_rs->setCityMap(&g_map);
 
   g_is = new InputSystem(g_rs);
-  g_is->setCityMap(&g_map);
+  //g_is->setCityMap(&g_map);
 
   g_map.generate();
 
+  g_mapRenderer.m_cityMap = &g_map;
+  g_mapRenderer.m_rs = g_rs;
+  g_mapRenderer.loadMeshes();
+  g_mapRenderer.generateQueue();
+
+  // Window size callback
+  glfwSetWindowSizeCallback(g_windowManager.g_window, window_size_callback);
+  glfwSetFramebufferSizeCallback(g_windowManager.g_window, window_size_callback);
+
   // Cursor callbacks
-  glfwSetCursorPosCallback(g_window, cursor_pos_callback);
-  glfwSetMouseButtonCallback(g_window, mouse_button_callback);
-  glfwSetScrollCallback(g_window, scroll_callback);
+  glfwSetCursorPosCallback(g_windowManager.g_window, cursor_pos_callback);
+  glfwSetMouseButtonCallback(g_windowManager.g_window, mouse_button_callback);
+  glfwSetScrollCallback(g_windowManager.g_window, scroll_callback);
+
+  // TODO Touch callbacks
+
+
+  // HACK Render fixed loading
+  // g_meshGenerator.load_obj("data/Generator.obj");
+  // g_meshGenerator.upload_obj();
+
+  // g_meshTree.load_obj("data/lightmap/tree_lightmap.obj");
+  // g_meshTree.upload_obj();
+  //
+  // g_meshHouse.load_obj("data/lightmap/house1_lightmap.obj");
+  // g_meshHouse.upload_obj();
+  //
+  // g_renderQueue.setProgram(g_rs->programObject);
+  //
+  // // g_renderQueue.setMesh(&g_meshGenerator);
+  // // g_renderQueue.setMVP(glm::mat4(1.0f));
+  // // g_renderQueue.submit();
+  // //
+  // // g_renderQueue.setMVP(glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 1.f, 0.f)));
+  // // g_renderQueue.submit();
+  //
+  // g_renderQueue.setMesh(&g_meshTree);
+  // g_renderQueue.setMVP(glm::translate(glm::mat4(1.0f), glm::vec3(1.f, 0.f, 0.f)));
+  //
+  // g_renderQueue.setMesh(&g_meshHouse);
+  // g_renderQueue.setMVP(glm::translate(glm::mat4(1.0f), glm::vec3(1.f, 1.f, 0.f)));
+  //
+  //
+  // static Texture g_texture;
+  // static Texture g_texture2;
+  //
+  // g_texture.loadPng("data/lightmap/lightmap.png");
+  // g_texture2.loadPng("data/lightmap/lightmap_house1.png");
+  //
+  // g_meshTree.texture = &g_texture;
+  // g_meshHouse.texture = &g_texture2;
+
 
   return 0;
 }
--- makefile
+++ makefile
@@ -1,14 +1,19 @@
 CC=emcc
-SOURCES:=main.cpp $(wildcard src/*.cpp)
+SOURCES:=main.cpp $(wildcard src/*.cpp) $(wildcard ../WebCore/src/*.cpp) $(wildcard ../WebCore/imgui/*.cpp)
 LDFLAGS=-O2 --llvm-opts 2
 OUTPUT=out/themayor.js
+USE_IMGUI=-I../WebCore/imgui/
+USE_VORBIS=-s USE_VORBIS=1
+USE_LIBPNG=-s USE_LIBPNG=1 -s USE_ZLIB=1 
+USE_WEBGL=-s FULL_ES3=1 -s USE_GLFW=3 -s USE_WEBGL2=1 -lGLU -lGL
+USE_ENGINE_WEBCORE=-I../WebCore/include/
 
 #-s USE_WEBGL2=1 
 
 all: $(SOURCES) $(OUTPUT)
 
 $(OUTPUT): $(SOURCES) 
-	$(CC) -Isrc -I/usr/local/include/ $(SOURCES) --bind -s FULL_ES3=1 -std=c++11 $(LDFLAGS) -o $(OUTPUT) -s WASM=1 -s USE_GLFW=3 -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=1 -s USE_WEBGL2=1 -lGLU -lGL --preload-file data
+	$(CC) -Isrc -I/usr/local/include/ $(SOURCES) $(USE_IMGUI) $(USE_VORBIS) $(USE_LIBPNG) $(USE_WEBGL) $(USE_ENGINE_WEBCORE) --bind -std=c++11 $(LDFLAGS) -o $(OUTPUT) -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=1 --preload-file data
 
 clean:
 	rm $(OUTPUT)
